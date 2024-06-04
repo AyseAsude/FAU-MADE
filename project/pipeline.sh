66690fb9e22acc6342bd58c3001add7f86c1cdf7
AyseAsude <ayasude99@hotmail.com>
2024-06-04 20:53:00 +0200

    added new script to the pipeline

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# please provide the path to the file extract_transform.py
-file_path="path_to_the_project\project\extract_transform.py" 
-python "$file_path"
+# please provide the path to the file extract_transform.py and selenium_load_2020.py
+selenium_path="C:\Users\asude\Desktop\FAU Summer Semester 2024\MADE\FAU-MADE\project\selenium_load_2020.py"
+extract_transform_path="C:\Users\asude\Desktop\FAU Summer Semester 2024\MADE\FAU-MADE\project\extract_transform.py"
+
+python "$selenium_path"
+python "$extract_transform_path"
